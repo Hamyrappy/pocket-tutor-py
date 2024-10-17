8818850108fb4c1bea3b12e0384f1ae52f1b190a
OstarkovSN <ostarkovsn@gmail.com>
2024-10-17 16:32:09 +0300

    created main.py template

--- main.py
+++ main.py
@@ -1,27 +1,57 @@
 import os
 
 import pandas as pd
-from dotenv import load_dotenv
-
-from app.models.yandexgpt import YandexGPT
+from models import *
 from app.utils.submit import generate_submit
+import json
 
 if __name__ == "__main__":
-    load_dotenv()
 
-    system_prompt = """
-    Ты - профессиональный программист и ментор. Давай очень короткие ответы о синтаксических ошибках в коде, если они есть.
-    """
+    data = json.load(open("data/processed/test/prepared.json", "r"))
+    librarian = Librarian(???)
+    formatter = Formatter(???)
+    commentator = Commentator(???)
+    template = '''?????????????
+УСЛОВИЕ ЗАДАЧИ:
+{task}
 
-    yandex_gpt = YandexGPT(
-        token=os.environ["YANDEX_GPT_IAM_TOKEN"],
-        folder_id=os.environ["YANDEX_GPT_FOLDER_ID"],
-        system_prompt=system_prompt,
-    )
+ОБРАЗЕЦ правильного решения:
+{correct_example}
 
+НЕПРАВИЛЬНОЕ РЕШЕНИЕ ученика:
+{student_solution}
 
+ПРИМЕРЫ комментария:
+1.
+{comments[0]}
+
+2.
+{comments[1]}
+
+3.
+{comments[2]}
+
+4.
+{comments[3]}'''
     def predict(row: pd.Series) -> str:
-        return yandex_gpt.ask(row["student_solution"])
+        solution_id = row['id']
+        info = data[str(solution_id)]
+        task = info['task']
+        student_solution = info['student_solution']
+        author_solution = info['author_solution']
+        test_system_report = info['test_system_report']
+        other_comments = librarian(???)
+        prompt = template.format(
+            task=task,
+            student_solution=student_solution,
+            author_solution=author_solution,
+            test_system_report=test_system_report,
+            comments=other_comments
+        )
+        model_output_raw = commentator(prompt)
+        model_output = formatter(model_output_raw)
+        return model_output
+        
 
 
     generate_submit(
