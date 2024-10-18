7ff5e88ba60a8f5a4c75ce3d9cca60c2beccd0a9
Hamyrappy <k.v.t10@mail.ru>
2024-10-18 14:55:12 +0300

    Добавил сырые данные и обновил agentic_system

--- main.py
+++ main.py
@@ -5,14 +5,15 @@ from agentic_system import AngenticSystem
 from app.utils.submit import generate_submit
 import json
 from scripts.run_tester import run_tester
+from agentic_system.prompts import code_analysis_template_2, comment_writer_template_2
 
 if __name__ == "__main__":
     from dotenv import load_dotenv
-    load_dotenv(dotenv_path="env.env")
+    load_dotenv()
         
     #run_tester() #-- для запуска тестировщика. Работает долго, запускайте один раз. Уже выполнен.
     data = json.load(open("data/processed/test/prepared.json", "r", encoding="utf-8"))
-    ag_sys = AngenticSystem(os.environ["SAMBANOVA_API_KEY"])
+    ag_sys = AngenticSystem(os.environ["SAMBANOVA_API_KEY"],comment_writer_template=comment_writer_template_2,code_analysis_template=code_analysis_template_2)
 
     def predict(idx):
         info = data[idx]
