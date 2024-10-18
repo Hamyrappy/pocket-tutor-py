5ab0c3ce7221f03a5665c7cc65396a85afe9cafa
OstarkovSN <ostarkovsn@gmail.com>
2024-10-18 23:15:26 +0300

    added main_single

---
+++ main_single.py
@@ -0,0 +1,26 @@
+from utils.datamanager import Solution
+from utils import DataManager
+import agentic_system.prompts as prompts
+from main import ag_sys
+from utils import Tester
+from utils.processing import _predict_error_message
+
+def predict_single(code, task_id, data_type='test'):
+    solution = Solution({
+        'student_solution': code,
+        'task_id': task_id,
+        'id': -1,
+        'author_comment': 'None'})
+    dm = DataManager('data/processed/{}'.format(data_type))
+    task = dm.get_task(int(task_id))
+    tester = Tester(task)
+    tester_results= solution.test(tester)
+    error_message = _predict_error_message(tester_results)
+    return ag_sys.predict(task, task.author_solution, code, tester_results['report'], error_message)
+
+if __name__ == "__main__":
+    code = """"""
+    task_id = '4'
+    data_type = 'test'
+    
+    print(predict_single(code, task_id, data_type))
