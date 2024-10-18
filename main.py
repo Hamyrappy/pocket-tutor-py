1941257cb532b78b9b5cfc7d600b0f751b07306a
OstarkovSN <ostarkovsn@gmail.com>
2024-10-18 11:34:30 +0300

    corrected submission

--- main.py
+++ main.py
@@ -7,13 +7,12 @@ import json
 from scripts.run_tester import run_tester
 
 if __name__ == "__main__":
-    run_tester() #-- для запуска тестировщика. Работает долго, запускайте один раз. Уже выполнен.
+    #run_tester() #-- для запуска тестировщика. Работает долго, запускайте один раз. Уже выполнен.
     data = json.load(open("data/processed/test/prepared.json", "r", encoding="utf-8"))
     ag_sys = AngenticSystem(os.environ["SAMBANOVA_API_KEY"])
 
-    def predict(row: pd.Series):
-        solution_id = row['id']
-        info = data[str(solution_id)]
+    def predict(idx):
+        info = data[idx]
         model_output = ag_sys.predict(**info)
         return model_output
         
